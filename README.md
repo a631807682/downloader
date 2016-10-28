0c5cf23f322f365a3a017ee5282834ef1b8467ec
chenrui <631807682@qq.com>
2016-10-28 10:32:57 +0800

    first

---
+++ README.md
@@ -0,0 +1,69 @@
+download
+------------------
+#使用方法
+>npm install https://github.com/a631807682/downloader.git
+
+```
+const Download = require('../index');
+
+let options = {
+    rejectUnauthorized: false,
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": 0,
+        "ei": "BFEBFBFF-000206A7-00000000-00000000+WD-WXA1A91X2323",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1.7601; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36",
+        "token": "3zRqTuPmjGWA+lZxI/JVGcuIemnXpdpSV2tsCl0xd59ARQ5EmR1gIdulPXk8LFY0vkDJVVaEKK2/UpGERM4kKKrAsQBccFepR02kR74TU84=",
+        "language": "zh-CN"
+    }
+};
+
+// let url = 'http://www.sqlite.org/2016/sqlite-dll-win64-x64-3130000.zip';
+let url = 'http://192.168.1.205:10060/application/download/5811c2bce31e6e1aa94db138'; 
+// let url = 'https://gw.alicdn.com/bao/uploaded/LB1vovgMVXXXXXaXVXXXXXXXXXX.zip';
+
+let download = new Download(url, options, { 'abc': 'hahaha' });
+
+
+const handle = {
+    /*
+        取消
+     */
+    destroyed: function(module) {
+        console.log('Download destroyed.');
+    },
+    /*
+        出错
+     */
+    error: function(err, module) {
+        console.log('Download  error : ' + err);
+    },
+    /*
+        正在下载
+     */
+    started: function(state, module) {
+        console.log('Download progress: ' + state.progress + ' %');
+    },
+    /*
+        下载成功
+     */
+    downloadFinished: function(module) {
+        console.log('Download completed');
+        // download.destroy();
+    },
+    /*
+        下载并解压成功
+     */
+    finished: function(module) {
+        console.log('Download and unzip completed !');
+    }
+};
+
+
+download.start('./output', handle);
+
+setTimeout(function() {
+    // download.destroy();
+},1000);
+
+```
---
+++ index.js
@@ -0,0 +1,110 @@
+const fs = require('fs'),
+    request = require('request'),
+    progress = require('request-progress'),
+    unzip = require('unzip2'),
+    defaultHandle = require('./lib/defaultHandle');
+
+//临时文件目录
+let tempDirectory = './downloadtemp';
+
+class Downloader {
+    constructor(url, options, privateModule) {
+        options = options || {};
+        privateModule = privateModule || {};
+
+        //创建临时文件目录
+        if (!fs.existsSync(tempDirectory)) {
+            fs.mkdirSync(tempDirectory);
+        }
+
+        this._req = request(url, options); //request实例
+        this._privateModule = privateModule; //自定义标记
+        this._tempPath = `${tempDirectory}/${new Date().getTime()}.zip`; //临时文件
+        this._savePath = ''; //下载路径
+        this._unzipStream = null; //解压流
+        this._handle = defaultHandle; //操作行为
+    }
+
+    /**
+     * 开始下载
+     * @param  {[type]} savePath [保存地址]
+     * @param  {[type]} handle   [触发行为]
+     */
+    start(savePath, handle) {
+        let self = this;
+        self._savePath = savePath;
+        self._handle = handle = Object.assign(self._handle, handle);
+        let privateModule = self._privateModule; //自定义标记
+        let tempPath = self._tempPath; //临时文件
+
+        progress(self._req, { throttle: 1000 })//throttle the progress event to 1000ms
+            .on('response', (res) => {
+                if (res.headers['content-type'].includes('application/json')) { //服务自定义错误
+                    res.setEncoding('utf-8');
+                    res.on('data', function(data) {
+                        handle.error('server error ' + data, privateModule);
+                        self._req.abort(); //结束request
+                    });
+                }
+            })
+            .on('progress', (state) => {
+
+                let formatState = {
+                    progress: parseFloat(state.percentage * 100).toFixed(2)
+                };
+                //下载中事件
+                handle.started(formatState, privateModule);
+            })
+            .on('error', (err) => {
+                //错误事件
+                handle.error(err, privateModule);
+            })
+            .on('end', () => {
+                if (fs.existsSync(tempPath)) {
+
+                    //解压实例
+                    let unziper = unzip.Extract({ path: savePath });
+                    unziper.on('close', () => {
+                        //解压完成事件
+                        handle.finished(privateModule);
+                    });
+                    // 需要解压的文件流
+                    let unzipFile = self._unzipStream = fs.createReadStream(tempPath);
+
+                    //下载完成事件
+                    handle.downloadFinished(privateModule);
+                    // console.log('start unzip...');
+                    unzipFile.pipe(unziper); //开始解压
+                    // console.log('unlink unzip...');
+                    try {
+                        fs.unlinkSync(tempPath); //尝试删除 
+                    } catch (err) {
+                        // console.log('unlink...',err);
+                    }
+
+                } else {
+                    //下载未完成时abort
+                }
+            })
+            .on('abort', () => {
+                if (fs.existsSync(tempPath)) { //下载过程中取消
+                    if (self._unzipStream) { //解压过程中取消
+                        // console.log('abort unzipFile close');
+                        //结束解压
+                        self._unzipStream.close();
+                    }
+                    // console.log('abort fs unlink');
+                    fs.unlink(tempPath);
+                }
+                handle.destroyed(privateModule);
+            })
+            .pipe(fs.createWriteStream(tempPath));
+    }
+
+    destroy() {
+        this._req.abort();
+    }
+
+};
+
+module.exports = Downloader;
